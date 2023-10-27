e2b4027d3e4f32d126c2cbd81c98454a8cbaa0bc
yusuke-sudou <143806854+yusuke-sudou@users.noreply.github.com>
2023-10-27 05:57:19 +0000

    [Add]５章作成

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+$(function (){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
 });
-
